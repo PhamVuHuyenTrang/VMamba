0a48b249e654e4b3ecbe3afe5bb60107991bf329
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2024-09-02 17:31:46 +0700

    Update run.sh

--- classification/run.sh
+++ classification/run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="0, 1, 2"
 export PYTHONPATH="."
 
-python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=1 --master_addr="127.0.0.1" --master_port=29501 main.py --cfg /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_1/classification/configs/vssmab/vmambav0_tiny_224_a0.yaml --batch-size 32 --data-path /lustre/scratch/client/vinai/users/trangpvh1/imagenet --output /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_1/classification 
+python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=3 --master_addr="127.0.0.1" --master_port=29501 main.py --cfg /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_278/classification/configs/vssmab/vmambav0_tiny_224_a0.yaml --batch-size 128 --data-path /lustre/scratch/client/vinai/users/trangpvh1/imagenet --output /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_278/classification --resume /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_278/classification/vssm_tiny_v0/20240827180640/ckpt_epoch_3.pth 
