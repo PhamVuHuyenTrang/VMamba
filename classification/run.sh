002ff35c802b799803d8d374b50bc4777d4922c9
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2024-09-20 21:45:32 +0700

    Update run.sh

--- classification/run.sh
+++ classification/run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES="0, 1, 2"
+export CUDA_VISIBLE_DEVICES="0, 1, 2, 3, 4, 5, 6, 7"
 export PYTHONPATH="."
 
-python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=3 --master_addr="127.0.0.1" --master_port=29501 main.py --cfg /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_278/classification/configs/vssmab/vmambav0_tiny_224_a0.yaml --batch-size 128 --data-path /lustre/scratch/client/vinai/users/trangpvh1/imagenet --output /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_278/classification --resume /lustre/scratch/client/vinai/users/trangpvh1/repo/vinai_prj/VMamba_Global_local_278/classification/vssm_tiny_v0/20240827180640/ckpt_epoch_3.pth 
+python -m torch.distributed.launch --nnodes=1 --node_rank=0 --nproc_per_node=8 --master_addr="127.0.0.9" --master_port=10002 main.py --cfg /home/ubuntu/trang/repo/VMamba_global_local_lr/classification/configs/vssm/vmambav2v_tiny_224.yaml  --batch-size 128 --data-path /home/ubuntu/data/imagenet --output /home/ubuntu/trang/repo/VMamba_global_local_lr/classification/
